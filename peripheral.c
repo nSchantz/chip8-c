40b6166e643f04bf565beed8b3467cb5896a9829
Nicholas Schantz <nickjohnschantz@gmail.com>
2023-12-27 00:30:10 -0500

    !STILL BROKEN! Adding upscaling to avoid having to "get gud" at sdl

--- peripheral.c
+++ peripheral.c
@@ -18,8 +18,8 @@ sPeriph* initScreen() {
     SDL_Window *screen = SDL_CreateWindow("Chip8 - Interpreter",
             SDL_WINDOWPOS_UNDEFINED,
             SDL_WINDOWPOS_UNDEFINED,
-            SCREEN_WIDTH, SCREEN_HEIGHT,
-            SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE);
+            (SCREEN_WIDTH * SCREEN_RES_UPSCALE), (SCREEN_HEIGHT * SCREEN_RES_UPSCALE),
+            (SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE));
     if(!screen) 
     {
         fprintf(stderr, "SDL2 | Could not create window\n");
@@ -34,7 +34,7 @@ sPeriph* initScreen() {
     psPeriph->window = screen;
     psPeriph->renderer = renderer;
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
     SDL_Delay(3000);
@@ -44,6 +44,7 @@ sPeriph* initScreen() {
 
 void updateScreen(sPeriph* psPeriph, sMem* psMem) {
     uint8_t* pGrid = psMem->pFrameBufSec;
+    SDL_Rect* pPixel = (SDL_Rect*)malloc(sizeof(SDL_Rect));
 
     for (int row = 0; row < SCREEN_HEIGHT; row++) {
         for (int col = 0; col < (SCREEN_WIDTH / 8); col++) { // 1 col = 8 bits (or 8 pixels)
@@ -58,14 +59,30 @@ void updateScreen(sPeriph* psPeriph, sMem* psMem) {
                 {
                     SDL_SetRenderDrawColor(psPeriph->renderer, 0, 0, 0, 255);
                 }
-                SDL_RenderDrawPoint(psPeriph->renderer, (col * (SCREEN_WIDTH / 8)) + bit, row);
+                // TODO: Try to malloc at start of function and just reassign values here instead of remallocing.
+                //pPixel = (SDL_Rect*)malloc(sizeof(SDL_Rect));
+                // this shouldnt clip right?
+                pPixel->x = ((col * (SCREEN_WIDTH / 8)) + bit) * SCREEN_RES_UPSCALE;
+                pPixel->y = row * SCREEN_RES_UPSCALE;
+                pPixel->w = SCREEN_RES_UPSCALE;
+                pPixel->h = SCREEN_RES_UPSCALE; 
+                //SDL_RenderDrawPoint(psPeriph->renderer, (col * (SCREEN_WIDTH / 8)) + bit, row);
+                SDL_RenderFillRect(psPeriph->renderer, pPixel);
             }
+            pGrid = pGrid + SCREEN_WIDTH / 8;
         }
-        pGrid = pGrid + SCREEN_HEIGHT;
+        //pGrid = pGrid + SCREEN_WIDTH;
+
+        if (pGrid > psMem->pFrameBufSec + 0x100) {
+            printf("Vid  | Attempting to access bytes outside of frame buffer. %p >= %p + 0x100\n", pGrid, psMem->pFrameBufSec);
+            //return;
+        }
     }
 
+    // TODO: Try to free pPixel after re-rendering
     SDL_RenderPresent(psPeriph->renderer);
-    SDL_Delay(3000);
+    free(pPixel);
+    SDL_Delay(1000);
 }
 
 void closeScreen(sPeriph* psPeriph) {
--- ref/peripheral.h
+++ ref/peripheral.h
@@ -3,6 +3,7 @@
 
 #define SCREEN_WIDTH 64
 #define SCREEN_HEIGHT 32
+#define SCREEN_RES_UPSCALE 10 // (64x32)*10
 
 typedef struct sPeriph {
     SDL_Window* window;    
